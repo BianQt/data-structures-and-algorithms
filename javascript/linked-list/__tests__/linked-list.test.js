ad7658297111cc23aa3d9d3e3ca90e927084db18
BianQt <bayanqutshan@gmail.com>
2021-11-04 19:21:57 +0200

    Challange05 Complete

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,49 @@
-'use strict';
+"use strict";
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require("../lib/LinkedList");
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+let list = new LinkedList();
+
+describe("Linked list", () => {
+  it("creation of linkedList works", () => {
+    expect(list.head).toBeNull();
   });
+
+  it("insert to linkedList works", () => {
+    list.insert("a");
+    expect(list.head.value).toEqual("a");
+  });
+
+  // The head property will properly point to the first node in the linked list
+  it("head property", () => {
+    expect(list.head.value).toEqual("a");
+  });
+
+  // Can properly insert multiple nodes into the linked list
+  it("insert multiple nodes", () => {
+    list.insert("b");
+    expect(list.tail.value).toEqual("b");
+    list.insert("c");
+    expect(list.tail.value).toEqual("c");
+  });
+  
+  // Will return true when finding a value within the linked list that exists
+  it("finding a value within the linked list", () => {
+    let index = list.include("b");
+    expect(typeof index).toEqual("number");
+  });
+
+  // Will return false when searching for a value in the linked list that does not exist
+  it("finding a value thath does not exist within the linked list", () => {
+    let index = list.include("f");
+    expect(index).toEqual(null);
+  });
+
+  // Can properly return a collection of all the values that exist in the linked list
+  it("collection of all the values that exist in the linked list", () => {
+    let string = list.string();
+    expect(string).toEqual("{ a } -> { b } -> { c } -> NULL");
+  });
+
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-class LinkedList {
+const LinkedList = require('./lib/LinkedList');
 
-}
+// create a new instance of list
+const list= new LinkedList(); 
 
-module.exports = LinkedList;
+list.insert('a');
+list.insert('b');
+list.insert('c');
+list.insert('d');
+list.insert('e');
+list.insert('f');
+
+console.log(list);
